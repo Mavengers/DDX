f15aa937a6fa6a0d6dbd47a24bdb6f1a219744c0
Haobot <haobot@foxmail.com>
2023-08-13 17:22:36 +0800

    add MPU6050 support

--- DonkeyDriveX/DonkeyDriveX.ino
+++ DonkeyDriveX/DonkeyDriveX.ino
@@ -1,6 +1,10 @@
 // #include <esp_now.h>
 // #include <WiFi.h>
 #include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_MPU6050 mpu;
 
 // #define DEBUG  // Uncomment to enable debugging output
 
@@ -172,23 +176,124 @@ void read_ina219(){
   Serial.printf("%d: %f V\r\n", 2, I2CReadValue(0x41, 2) / 2 / 1000.0);
 }
 
+void read_mpu6050(){
+/* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+}
 
 void setup() {
 	Serial.begin(115200);
   Wire.begin(2, 3, 400000L); // SDA, SCL, 400kHz
-    // Set the RC receiver pins as inputs and attach the interrupts
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  // set accelerometer Range
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  // set Gyro Range
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+  // Set filter bandwidth
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+
+  // Set the RC receiver pins as inputs and attach the interrupts
   for (int i = 0; i < 4; i++) {
     pinMode(CH1_PIN + i, INPUT);
     attachInterrupt(digitalPinToInterrupt(CH1_PIN + i), isr_functions[i], CHANGE);
   }
-
 
   ledcSetup(CH_STEERING, 50, 14); // 定义通道0（steering）
   ledcSetup(CH_THROTTLE, 50, 14); // 定义通道1（Throttle）PWM输出为pin1，50Hz，14-bit resolution
 	// 分配PWM输出通道到管脚	
   ledcAttachPin(STEERING_PIN, CH_STEERING);
   ledcAttachPin(THROTTLE_PIN, CH_THROTTLE);
-  
+
 	// WiFi.mode(WIFI_STA); 
   // for(int i = 0; i < 10; i++)
   //   Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
@@ -199,12 +304,15 @@ void setup() {
 	// }
 
 	// esp_now_register_recv_cb(OnDataRecv);
+  
+  //SETUP ENDS
 }
 
 
 void loop() {
   //  Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
   read_ina219();
+  read_mpu6050();
 
   if (Serial.available()){
       String CMD = Serial.readStringUntil('\n');
